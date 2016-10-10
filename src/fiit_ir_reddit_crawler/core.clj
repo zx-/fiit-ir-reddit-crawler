dc934d0e568148f13a712bd764c6de2abd62e746
Robert Cuprik <robertcuprik@gmail.com>
2016-10-10 17:18:30 +0200

    Add clj-http dependency

--- src/fiit_ir_reddit_crawler/core.clj
+++ src/fiit_ir_reddit_crawler/core.clj
@@ -1,5 +1,8 @@
 (ns fiit-ir-reddit-crawler.core)
 
+(def subbredit-root-xml "http://reddit-sitemaps.s3-website-us-east-1.amazonaws.com/subreddit-sitemaps.xml")
+(def threads-root "http://reddit-sitemaps.s3-website-us-east-1.amazonaws.com/comment-page-sitemaps.xml")
+
 (defn foo
   "I don't do a whole lot."
   [x]
