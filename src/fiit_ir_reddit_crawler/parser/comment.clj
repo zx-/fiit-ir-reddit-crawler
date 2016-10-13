850fe7b6942097b9880feb62423b2e3ebf0d18f5
Robert Cuprik <robertcuprik@gmail.com>
2016-10-13 23:12:25 +0200

    Add comment title parsing

--- src/fiit_ir_reddit_crawler/parser/comment.clj
+++ src/fiit_ir_reddit_crawler/parser/comment.clj
@@ -16,8 +16,8 @@
 
 (defn get-title
   [title-res]
-  (html/text
-    (c/normalize-text
+  (c/normalize-text
+    (html/text
       (c/get-content title-res [:p.title :a.title]))))
 
 (defn get-score
@@ -36,6 +36,58 @@
       :attrs
       :href))
 
+(defn get-user-flair
+  [title-res]
+  (c/get-content title-res [:.flair]))
+
+; <link rel="canonical"
+(defn get-link
+  [resource]
+  (-> resource
+      (html/select [(html/attr= :rel "canonical")])
+      first
+      :attrs
+      :href))
+
+(defn get-subreddit
+  [resource]
+  (-> resource
+      (html/select [:#siteTable :> :.thing])
+      first
+      :attrs
+      :data-subreddit))
+
+(defn get-date
+  [title-res]
+  (-> title-res
+      (html/select [:time.live-timestamp])
+      first
+      :attrs
+      :datetime))
+
+(defn get-user-text
+  [title-res]
+  (if-let [e (first (html/select title-res [:form.usertext :> :.usertext-body]))]
+    (-> e html/text c/normalize-text)))
+
+(defn get-num-of-comments
+  [title-res]
+  (-> (c/get-content title-res [:.buttons :.first :.comments])
+      (clojure.string/split #" ")
+      (nth 0)
+      read-string))
+
+(defn get-target
+  [title-res]
+  (if-let [url (first (html/select title-res [:p.title :a.title.outbound]))]
+    (:data-href-url (:attrs url))))
+
+(defn is-media?
+  [title-res]
+  (if-let [media (first (html/select title-res [:.media-preview]))]
+    true
+    false))
+
 (defn parse-comment
   "returns hash of info from subreddit"
   [html-string]
@@ -47,4 +99,12 @@
         (assoc :title (get-title title-area))
         (assoc :score (get-score res))
         (assoc :user-name (get-user-name title-area))
-        (assoc :user-link (get-user-link title-area)))))
+        (assoc :user-link (get-user-link title-area))
+        (assoc :user-flair (get-user-flair title-area))
+        (assoc :link (get-link res))
+        (assoc :subreddit (get-subreddit res))
+        (assoc :date (get-date title-area))
+        (assoc :user-text (get-user-text title-area))
+        (assoc :num-of-comments (get-num-of-comments title-area))
+        (assoc :target (get-target title-area))
+        (assoc :media (is-media? title-area)))))
--- test/fiit_ir_reddit_crawler/parser/comment_test.clj
+++ test/fiit_ir_reddit_crawler/parser/comment_test.clj
@@ -15,10 +15,10 @@
    :user-name "AmGeophysicalU-AMA"
    :user-link "https://www.reddit.com/user/AmGeophysicalU-AMA"
    :user-flair "American Geophysical Union AMA Guest"
-   :link "/r/science/comments/579v6t/science_ama_series_hi_reddit_im_phil_klotzbach_a/"
+   :link "https://www.reddit.com/r/science/comments/579v6t/science_ama_series_hi_reddit_im_phil_klotzbach_a/"
    :subreddit "science"
    :date "2016-10-13T11:59:35+00:00"
-   :user-text "I am Phil Klotzbach, a research scientist in the Department of Atmospheric Science at Colorado State University. I worked for over 15 years with the late Dr. Bill Gray, a renowned scientist who conducted groundbreaking studies in hurricane genesis, structure and intensity change as well as pioneering Atlantic basin seasonal hurricane prediction. While our Tropical Meteorology Project is best known among the general public for the seasonal hurricane predictions, I conduct research on a variety of hurricane-related topics including shorter-term prediction as well as potential future changes in tropical cyclone activity driven both by natural variability as well as anthropogenic causes. I maintain a very active presence on social media through my Twitter feed (@philklotzbach) where I provide frequent updates on current global tropical cyclone activity and compare them with historical statistics. I also maintain global real-time hurricane statistics. In cooperation with the Barcelona Supercomputing Centre, I helped create a repository of all publicly-available seasonal hurricane forecasts for the Atlantic basin from various government agencies, universities and private forecasting companies.\nCurrently, I am working on a variety of research projects, including the generation of an updated global tropical cyclone climatology as well as a paper on the life and legacy of Dr. Gray. I am also closely monitoring the potential shift away from the active Atlantic hurricane era that we have been in since 1995. I was lead author on a paper last year that raised the question that we might be moving out of the active era for Atlantic hurricanes.\nI look forward to chatting with you about all things hurricane!\nI’ll be back at noon EST (9 am PST, 5 pm UTC) to answer your questions, ask me anything!"
+   :user-text "I am Phil Klotzbach, a research scientist in the Department of Atmospheric Science at Colorado State University. I worked for over 15 years with the late Dr. Bill Gray, a renowned scientist who conducted groundbreaking studies in hurricane genesis, structure and intensity change as well as pioneering Atlantic basin seasonal hurricane prediction. While our Tropical Meteorology Project is best known among the general public for the seasonal hurricane predictions, I conduct research on a variety of hurricane-related topics including shorter-term prediction as well as potential future changes in tropical cyclone activity driven both by natural variability as well as anthropogenic causes. I maintain a very active presence on social media through my Twitter feed (@philklotzbach) where I provide frequent updates on current global tropical cyclone activity and compare them with historical statistics. I also maintain global real-time hurricane statistics. In cooperation with the Barcelona Supercomputing Centre, I helped create a repository of all publicly-available seasonal hurricane forecasts for the Atlantic basin from various government agencies, universities and private forecasting companies. Currently, I am working on a variety of research projects, including the generation of an updated global tropical cyclone climatology as well as a paper on the life and legacy of Dr. Gray. I am also closely monitoring the potential shift away from the active Atlantic hurricane era that we have been in since 1995. I was lead author on a paper last year that raised the question that we might be moving out of the active era for Atlantic hurricanes. I look forward to chatting with you about all things hurricane! be back at noon EST (9 am PST, 5 pm UTC) to answer your questions, ask me anything!"
    :num-of-comments 86
    :media false
    :target nil})
@@ -31,7 +31,7 @@
    :user-name "Hugpandas"
    :user-link "https://www.reddit.com/user/Hugpandas"
    :user-flair nil
-   :link "/r/worldnews/comments/579q17/bob_dylan_has_been_awarded_the_2016_nobel_prize/"
+   :link "https://www.reddit.com/r/worldnews/comments/579q17/bob_dylan_has_been_awarded_the_2016_nobel_prize/"
    :subreddit "worldnews"
    :date "2016-10-13T11:18:01+00:00"
    :user-text nil
@@ -47,11 +47,11 @@
    :user-name "Ms_Virtualizza"
    :user-link "https://www.reddit.com/user/Ms_Virtualizza"
    :user-flair nil
-   :link "/r/gifs/comments/579km2/happiness/"
+   :link "https://www.reddit.com/r/gifs/comments/579km2/happiness/"
    :subreddit "gifs"
    :date "2016-10-13T10:28:11+00:00"
    :user-text nil
-   :num-of-comments 668
+   :num-of-comments 782
    :target "http://i.imgur.com/ik3lIft.gifv"
    :media true})
 
@@ -120,6 +120,95 @@
            (get-user-link wrldn-res)))
     (is (= (:user-link gif-attrs)
            (get-user-link gif-res)))))
+
+(deftest get-link-test
+  (let [geo-res (string->resource html-geo)
+        wrldn-res (string->resource html-worldnews)
+        gif-res (string->resource html-gif)]
+    (is (= (:link geo-attrs)
+           (get-link geo-res)))
+    (is (= (:link worldnews-attrs)
+           (get-link wrldn-res)))
+    (is (= (:link gif-attrs)
+           (get-link gif-res)))))
+
+(deftest get-user-flair-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:user-flair geo-attrs)
+           (get-user-flair geo-res)))
+    (is (= (:user-flair worldnews-attrs)
+           (get-user-flair wrldn-res)))
+    (is (= (:user-flair gif-attrs)
+           (get-user-flair gif-res)))))
+
+(deftest get-subreddit-test
+  (let [geo-res (string->resource html-geo)
+        wrldn-res (string->resource html-worldnews)
+        gif-res (string->resource html-gif)]
+    (is (= (:subreddit geo-attrs)
+           (get-subreddit geo-res)))
+    (is (= (:subreddit worldnews-attrs)
+           (get-subreddit wrldn-res)))
+    (is (= (:subreddit gif-attrs)
+           (get-subreddit gif-res)))))
+
+(deftest get-date-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:date geo-attrs)
+           (get-date geo-res)))
+    (is (= (:date worldnews-attrs)
+           (get-date wrldn-res)))
+    (is (= (:date gif-attrs)
+           (get-date gif-res)))))
+
+(deftest get-user-text-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:user-text geo-attrs)
+           (get-user-text geo-res)))
+    (is (= (:user-text worldnews-attrs)
+           (get-user-text wrldn-res)))
+    (is (= (:user-text gif-attrs)
+           (get-user-text gif-res)))))
+
+(deftest get-num-of-comments-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:num-of-comments geo-attrs)
+           (get-num-of-comments geo-res)))
+    (is (= (:num-of-comments worldnews-attrs)
+           (get-num-of-comments wrldn-res)))
+    (is (= (:num-of-comments gif-attrs)
+           (get-num-of-comments gif-res)))))
+
+(deftest get-target-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:target geo-attrs)
+           (get-target geo-res)))
+    (is (= (:target worldnews-attrs)
+           (get-target wrldn-res)))
+    (is (= (:target gif-attrs)
+           (get-target gif-res)))))
+
+(deftest is-media?-test
+  (let [geo-res (get-title-area (string->resource html-geo))
+        wrldn-res (get-title-area (string->resource html-worldnews))
+        gif-res (get-title-area (string->resource html-gif))]
+    (is (= (:media geo-attrs)
+           (is-media? geo-res)))
+    (is (= (:media worldnews-attrs)
+           (is-media? wrldn-res)))
+    (is (= (:media gif-attrs)
+           (is-media? gif-res)))))
+
 
 
 (deftest parse-comment-test
